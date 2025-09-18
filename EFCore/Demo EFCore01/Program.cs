d4673fcb5562ce7c5937886e8296918675cf22da
Ahmednabil200300 <ahmed3029184@gmail.com>
2025-09-18 08:16:37 +0300

    Add files via upload

---
+++ EFCore/Demo EFCore01/Program.cs
@@ -0,0 +1,15 @@
+﻿using Demo_EFCore01.Contexts;
+using Microsoft.Identity.Client;
+
+namespace Demo_EFCore01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using itiDBContext dbcontext = new itiDBContext();
+            
+            
+        }
+    }
+}
