cbf6587bc0d2206493825056cc002965ef60547d
Ahmednabil200300 <ahmed3029184@gmail.com>
2025-09-23 06:02:48 +0300

    Add files via upload

---
+++ EFCore/EFCore02/Configuration/Course_InstConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using EFCORE02.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCORE02.Configuration
+{
+    internal class Course_InstConfiguration : IEntityTypeConfiguration<Course_Inst>
+    {
+        public void Configure(EntityTypeBuilder<Course_Inst> builder)
+        {
+            builder.HasKey(IC => new { IC.Inst_Id, IC.Course_Id });
+        }
+    }
+}
---
+++ EFCore/EFCore02/Configuration/Stud_CourseConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using EFCORE02.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCORE02.Configuration
+{
+    internal class Stud_CourseConfiguration : IEntityTypeConfiguration<Stud_Course>
+    {
+        public void Configure(EntityTypeBuilder<Stud_Course> builder)
+        {
+            builder.HasKey(SC => new { SC.Stud_Id, SC.Course_Id });
+        }
+    }
+}
