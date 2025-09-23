c08677b19abbe2ebf51aca6ee926acf236c46cce
Ahmednabil200300 <ahmed3029184@gmail.com>
2025-09-23 06:03:09 +0300

    Add files via upload

---
+++ EFCore/EFCore02/Program.cs
@@ -0,0 +1,12 @@
+﻿using EFCORE02.Context;
+
+namespace EFCORE02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using ITIDBContext dbcontext = new ITIDBContext();
+        }
+    }
+}
