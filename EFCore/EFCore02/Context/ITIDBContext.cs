29f9a0e37124aac2c890efd2f3dc6002672ec819
Ahmednabil200300 <ahmed3029184@gmail.com>
2025-09-23 06:02:33 +0300

    Add files via upload

---
+++ EFCore/EFCore02/Context/ITIDBContext.cs
@@ -0,0 +1,32 @@
+﻿using EFCORE02.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCORE02.Context
+{
+    internal class ITIDBContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server = . ;Database = ITIDBContext ; Trusted_Connection =true ; TrustServerCertificate =true");
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Topic> Topics { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+
+}
