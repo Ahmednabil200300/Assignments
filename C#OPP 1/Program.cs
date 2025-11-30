892e8724e1e3377cfa860c73c330160c34812f81
Ahmednabil200300 <ahmed3029184@gmail.com>
2025-12-01 01:55:48 +0200

    Add files via upload

---
+++ C#OPP 1/Program.cs
@@ -0,0 +1,89 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+
+namespace assignment
+{
+    internal class Program
+    {
+        public static void OrintWeekDay()
+        {
+           
+        }
+        static void Main(string[] args)
+        {
+            #region Q1
+        
+
+            //foreach (var day in Enum.GetValues(typeof(DayOfWeek)))
+            //{
+            //    Console.WriteLine(day);
+            //}
+
+
+            #endregion
+
+            #region Q2
+         
+            //Console.WriteLine("Enter a season name: ");
+
+            //SeasOn season;
+            //Enum.TryParse(Console.ReadLine(), out season);
+            //  switch (season)
+            //    {
+            //        case SeasOn.Spring:
+            //            Console.WriteLine("Rang: march to may");
+            //            break;
+            //        case SeasOn.Summer:
+            //            Console.WriteLine("Rang: June to August");
+            //            break;
+            //        case SeasOn.Autumn:
+            //            Console.WriteLine("Rang: September to November");
+            //            break;
+            //        case SeasOn.Winter:
+            //            Console.WriteLine("Rang: December to February");
+            //            break;
+            //    }
+
+
+
+
+            #endregion
+
+            #region Q3
+
+            //Permessions userPerm = Permessions.None; 
+            //Console.WriteLine("Adding permessions : ");
+            //userPerm = userPerm | Permessions.Read;  
+            //userPerm |= Permessions.Execute;//
+            //Console.WriteLine(userPerm);
+            //userPerm = userPerm ^ Permessions.Execute; 
+            //Console.WriteLine("Removing permessions");
+            //Console.WriteLine(userPerm);
+
+            
+
+            #endregion
+
+            #region Q4
+            //Console.WriteLine("Enter a Color name: ");
+
+            //Color color;
+            //Enum.TryParse(Console.ReadLine(), out color);
+            //switch (color)
+            //{
+            //    case Color.red:
+            //    case Color.green:
+            //    case Color.blue:
+            //        Console.WriteLine("the input color is a primary color");
+            //        break;
+            //    default:
+            //        Console.WriteLine("the input color is not a primary color");
+            //        break ;
+
+            //}
+
+            #endregion
+
+        }
+    }
+}
