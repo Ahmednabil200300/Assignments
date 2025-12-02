e449f164c75b0131db5e56d2fc7524e06ae746ac
Ahmednabil200300 <ahmed3029184@gmail.com>
2025-12-02 23:04:03 +0200

    Update Program.cs

--- C#OOP 3/assignment/Program.cs
+++ C#OOP 3/assignment/Program.cs
@@ -20,14 +20,7 @@
 
             // 3 
 
-            /* 3.Create an array of Employees with size three a DBA,
-             * Guest and the third one is security officer who have full permissions. (Employee [] EmpArr;)
-             * Implement All the Necessary Member Functions on the Class (Getters, Setters)
-                Define all the Necessary Constructors for the Class
-                Allow NO RUNTIME errors if the user inputs any data
-                Write down all the necessary Properties (Instead of setters and getters)
-
-             */
+            /* 3.
 
             //Employees[] employees = new Employees[3];
             //employees[0] = new Employees(1, "Ola" , SecurityPrivileges.DBA,3000, new HireDate(12,10, 2024) , Gender.F);
