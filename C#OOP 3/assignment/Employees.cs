a95d8ccaa34230fbfc9518ad5c970e8785ab9347
Ahmednabil200300 <ahmed3029184@gmail.com>
2025-12-02 23:02:59 +0200

    Add files via upload

---
+++ C#OOP 3/assignment/Employees.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment
+{
+    enum Gender
+    {
+        M, F
+    }
+    [Flags]
+    enum SecurityPrivileges : Byte
+    {
+        Guest = 1,
+        Developer = 2,
+        Secretary = 4,
+        DBA = 8,
+           SecurityOfficer = 16,
+
+
+    }
+    class Employees
+    {
+
+
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public SecurityPrivileges SecurityPrivileges { get; set; }
+        public decimal Salary { get; set; }
+        public HireDate HireDate { get; set; }
+
+        public Gender Gender { get; set; }
+
+        public Employees()
+        {
+
+        }
+
+        public Employees(int id, string? name, SecurityPrivileges securityPrivileges, decimal salary, HireDate hireDate, Gender gender)
+        {
+            Id = id;
+            Name = name;
+            SecurityPrivileges = securityPrivileges;
+            Salary = salary;
+            HireDate = hireDate;
+            Gender = gender;
+        }
+        public override string ToString()
+        {
+            return $"Id = {Id}\nName: {Name}\nSecurity Privileges: {SecurityPrivileges}\nSalary: {Salary}\nHire Date {HireDate} \nGender {Gender} ";
+        }
+
+
+
+       
+    
+
+ 
+
+
+    }
+}
---
+++ C#OOP 3/assignment/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // assignment 3 OOP
+            // 1 and 2
+
+            /*
+             * 
+             */
+
+
+            //Employees Emp1 = new Employees(10, "ola", SecurityPrivileges.Developer , 3000 , new HireDate(12, 10 , 2024), Gender.F);
+
+
+            //Console.WriteLine(Emp1);
+
+            // 3 
+
+            /* 3.Create an array of Employees with size three a DBA,
+             * Guest and the third one is security officer who have full permissions. (Employee [] EmpArr;)
+             * Implement All the Necessary Member Functions on the Class (Getters, Setters)
+                Define all the Necessary Constructors for the Class
+                Allow NO RUNTIME errors if the user inputs any data
+                Write down all the necessary Properties (Instead of setters and getters)
+
+             */
+
+            //Employees[] employees = new Employees[3];
+            //employees[0] = new Employees(1, "Ola" , SecurityPrivileges.DBA,3000, new HireDate(12,10, 2024) , Gender.F);
+            //employees[1] = new Employees(2, "Mohammed", SecurityPrivileges.Guest, 1500, new HireDate(8, 2, 2023), Gender.M);
+            //employees[2] = new Employees(3, "Nour", SecurityPrivileges.SecurityOfficer,2000, new HireDate(12, 3, 2024), Gender.M);
+
+            //foreach (Employees emp in employees) {
+            //    Console.WriteLine(emp);
+            
+            //}
+
+
+
+
+
+        }
+    }
+}
